1e8f1693c54a4d2528711ca90219a10461f165b2
ehsanranjbar <ehsan.ranjbar.2000@gmail.com>
2023-08-15 12:39:16 +0330

    Remove dice_loss

--- model/loss.py
+++ model/loss.py
@@ -7,13 +7,3 @@ def mse_loss(output, target):
 
 def bce_loss(output, target):
     return F.binary_cross_entropy(output, target)
-
-
-def dice_loss(output, target):
-    smooth = 1.
-
-    iflat = output.contiguous().view(-1)
-    tflat = target.contiguous().view(-1)
-    intersection = (iflat * tflat).sum()
-
-    return 1 - ((2. * intersection + smooth) / (iflat.sum() + tflat.sum() + smooth))
